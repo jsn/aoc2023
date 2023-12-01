fe523927371c24e00beebc163ac6c427f1afbeb1
dmitry kim <jason@nichego.net>
2023-12-01 01:28:43 +0100

    util: proper A* implementation

--- util.clj
+++ util.clj
@@ -1,8 +1,63 @@
 (ns util
-  (:require [clojure.test :refer :all]))
+  (:require
+    [clojure.test :refer :all]
+    [clojure.data.priority-map :as prio-map]
+    [astar.core :as astar]))
 
 (defn string->vector [s] (read-string (str \[ s \])))
 
 (deftest everything
   (testing "string->vector"
     (is (= (string->vector "10,20,30,-1") [10 20 30 -1]))))
+
+(defn astar [graph dist h start goal?]
+  (loop [{:keys [queue came-from] :as env}
+         {:queue (prio-map/priority-map start (h start))
+          :came-from {start [0 nil]}}]
+    (let [[cur _] (peek queue)
+          [gcur _] (came-from cur)]
+      (if (goal? cur)
+        (loop [cur cur
+               rv '()]
+          (let [rv (cons cur rv)
+                [_ prev] (came-from cur)]
+            (if-not prev
+              rv
+              (recur prev rv))))
+        (when (seq queue)
+          (recur
+            (reduce
+              (fn [env n]
+                (let [gn (+ gcur (dist cur n))
+                      [gprev _] (came-from n)]
+                  (if (and gprev (>= gn gprev))
+                    env
+                    (-> env
+                      (update :queue assoc n (+ gn (h n)))
+                      (update :came-from assoc n [gn cur])))))
+              (update env :queue pop)
+              (graph cur))))))))
+
+(deftest astar-test
+  (testing "basic"
+    (let [g1 {:a [:b :c :d]
+              :c [:e :f]
+              :f [:g]}]
+      (is (=
+           (astar g1 (constantly 1) (constantly 100) :a #{:g})
+           [:a :c :f :g])))))
+
+#_(let [g1 {:a [:b :c :d]
+              :c [:e :f]
+              :f [:g]}]
+      (astar/route g1 (constantly 1) (constantly 100) :a :g))
+
+#_(let [g2 {:a [:b :c]
+            :b [:d]
+            :c [:d]
+            :d [:e]}
+        h #(get {:d 1 :e 0 :c 30} % 25)
+        dist #(get {[:c :d] 20} [%1 %2] 25)]
+    [(astar/route g2 dist h :a :e)
+     (astar g2 dist h :a #{:e})])
+
